fad78190eeb85c4d7524cae62d78be720de25ee9
Jake <jakesmorrison4@gmail.com>
2022-03-21 17:03:00 -0600

    update

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_admin import Admin
 from flask_cors import CORS
+import orm
 
 app = Flask(__name__)
 CORS(app)
-# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-# admin = Admin(app, name='microblog', template_mode='bootstrap3')
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 from views import blueprints
 for bp in blueprints: 
